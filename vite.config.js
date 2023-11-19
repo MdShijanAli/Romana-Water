b7237f0c80b905002b7c32f9326e44445cca0d5d
MdShijanAli <freelancershijan@gmail.com>
2023-11-19 12:55:09 +0600

    vite.config.js file update

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'pages/about.html'),
+        contact: resolve(__dirname, 'pages/contact.html'),
+
+        style: resolve(__dirname, 'style.css'), 
+        tailwind: resolve(__dirname, 'tailwind.config.css'), 
+        main: resolve(__dirname, 'main.js'),
+      },
+    },
+  },
+})
