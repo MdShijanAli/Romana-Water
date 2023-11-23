469df04e4ce1acb582a07a36221d15ec6fc51d57
MdShijanAli <freelancershijan@gmail.com>
2023-11-23 18:44:26 +0600

    animation done

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
         index: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'pages/about.html'),
         contact: resolve(__dirname, 'pages/contact.html'),
+        career: resolve(__dirname, 'pages/career.html'),
+        companyProfile: resolve(__dirname, 'pages/company-profile.html'),
+        products: resolve(__dirname, 'pages/products.html'),
 
         style: resolve(__dirname, 'style.css'), 
         tailwind: resolve(__dirname, 'tailwind.config.js'), 
