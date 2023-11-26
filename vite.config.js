82d2b338ac25debf6ced58a4b3cddf4138c81e19
MdShijanAli <freelancershijan@gmail.com>
2023-11-26 13:19:23 +0600

    Modal Issue Fixed

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
         career: resolve(__dirname, 'pages/career.html'),
         companyProfile: resolve(__dirname, 'pages/company-profile.html'),
         products: resolve(__dirname, 'pages/products.html'),
+        privacyPolicy: resolve(__dirname, 'pages/privacy-policy.html'),
+        returnRefund: resolve(__dirname, 'pages/return-refund.html'),
+        termandcondition: resolve(__dirname, 'pages/termandcondition.html'),
 
         style: resolve(__dirname, 'style.css'), 
         tailwind: resolve(__dirname, 'tailwind.config.js'), 
